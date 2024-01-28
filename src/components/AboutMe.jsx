2be21b156b47ab08a1eaa92fc7c67d75b67c05f2
jonathanphill <jonathanphill@gmail.com>
2024-01-28 12:03:38 -0500

    Starting to work again on the project

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,4 @@
 import './AboutMe.css'
-import { useState, useEffect } from "react";
 import CircularProgress from './CircularProgress';
 const AboutMe = () => {
   
@@ -13,10 +12,11 @@ const AboutMe = () => {
         <div className="introText">
           <div className="introTextContainer">
             <h3>
-              I'm Kuljit, <br />
+              I&apos;m Kuljit, <br />
               Photographer & Videographer
             </h3>
             <p>
+              
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ex
               soluta itaque, tenetur quia ea error mollitia quos.{" "}
             </p>
@@ -28,19 +28,7 @@ const AboutMe = () => {
             </p>
           </div>
 
-          <div className='introSkillContainer'>
-            <ul className="introSkill">
-              <li>
-                <CircularProgress skill="Photography" percentage={95} />
-              </li>
-              <li>
-                <CircularProgress skill="Events" percentage={85} />
-              </li>
-              <li>
-                <CircularProgress skill="Videography" percentage={95} />
-              </li>
-            </ul>
-          </div>
+          
         </div>
       </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,43 @@
-import './Main.css'
-import caribou from '../assets/caribou.jpg';
-import blackBear from '../assets/blackBear.jpg';
-import downTheRoad from '../assets/downTheRoad.jpg';
+import "./Main.css";
+import caribou from "../assets/caribou.jpg";
+import blackBear from "../assets/blackBear.jpg";
+import downTheRoad from "../assets/downTheRoad.jpg";
 import lakeview from "../assets/lakeview.jpg";
-
+import lakeview1 from "../assets/PHOTO-2023-06-18-10-33-50.jpg";
+import lakeview2 from "../assets/PHOTO-2023-06-18-11-02-32.jpg";
+import Footer from './Footer'; // Import the Footer component
 
 const Main = () => {
   return (
-    <main className='wrapper mainGallery'>
-      <div className='gallery'>
-        <img src={caribou} alt="" />
-        <img src={blackBear} alt="" />
-        <img src={downTheRoad} alt="" />
-        <img src={lakeview} alt="" />
-      </div>
-    </main>
+    <>
+      <main className="wrapper mainGallery">
+        <div className="gallery">
+          <div className="galleryImage">
+            <img src={caribou} alt="image of caribou" />
+          </div>
+          <div className="galleryImage">
+            <img
+              src={downTheRoad}
+              alt="long shot of road with hill in the backgorund"
+            />
+          </div>
+          <div className="galleryImage">
+            <img src={lakeview} alt="view of lake" />
+          </div>
+          <div className="galleryImage center-image">
+            <img src={lakeview1} alt="image of a blackbear" />
+          </div>
+          <div className="galleryImage center-image">
+            <img src={blackBear} alt="image of a blackbear" />
+          </div>
+          <div className="galleryImage center-image">
+            <img src={lakeview2} alt="image of a blackbear" />
+          </div>
+        </div>
+      </main>
+      <Footer/>
+    </>
   );
-}
+};
 
 export default Main;
