2b865ccf5acfee16c122d2d43d8b56b67e70554d
jonathanphill <jonathanphill@gmail.com>
2024-04-14 07:51:57 -0400

    added new html for footer and styling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
+import './ContactForm.css';
+import Footer from './Footer'; // Import the Footer component
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -34,41 +36,26 @@ const ContactForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </div>
+    <div className="formContainer wrapper">
+      <form action="/my-handling-form-page" method="post">
+        <ul>
+          <li>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="user_name" />
+          </li>
+          <li>
+            <label htmlFor="mail">Email:</label>
+            <input type="email" id="mail" name="user_email" />
+          </li>
+          <li>
+            <label htmlFor="msg">Message:</label>
+            <textarea id="msg" name="user_message"></textarea>
+          </li>
+        </ul>
+      </form>
 
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-        />
-      </div>
-
-      <button type="submit">Submit</button>
-    </form>
+      <Footer />
+    </div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,3 +34,5 @@ const Header = () => {
   );
 };
 export default Header;
+
+
